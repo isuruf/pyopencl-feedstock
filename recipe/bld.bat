eea681bb7bdd0a45dc698b348e720f4786ad0730
Isuru Fernando <isuruf@gmail.com>
2017-12-01 10:00:30 -0600

    Use the 32-bit MSVC2015 compiler for 64-bit code generation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,9 @@ if "%CONDA_PY%"=="27" (
   set COMPILER_FLAG=--compiler=mingw32
 ) else (
   set COMPILER_FLAG=
+  if "%ARCH%" == "64" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+  )
 )
 "%PYTHON%" configure.py --cl-inc-dir=%LIBRARY_INC% --cl-lib-dir=%LIBRARY_LIB%
 if errorlevel 1 exit 1
